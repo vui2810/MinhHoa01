fcbe796a890c4c73d56a8e98034e5750d1905001
ADMIN <ADMIN@PC219>
2024-05-28 10:01:25 +0700

    Add project files.

---
+++ MinhHoa01/Models/MayTinhModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhHoa01.Models
+{
+    public class MayTinhModel
+    {
+        public double So1 { get; set; }
+
+        public double So2 { get; set; }
+
+        public string PhepTinh { get; set; }
+
+        public double KetQua()
+        {
+            double ketqua = 0;
+            switch (PhepTinh)
+            {
+                case "+": ketqua = So1 + So2; break;
+                case "-": ketqua = So1 - So2; break;
+                case "*": ketqua = So1 * So2; break;
+                case "/": ketqua = So1 / So2; break;
+            }
+            return ketqua;
+            
+        }
+    }
+}
